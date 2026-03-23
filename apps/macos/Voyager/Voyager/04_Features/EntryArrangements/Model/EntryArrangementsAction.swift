36bcbcbead4c8999720855c13549517ed3731b08
Jongmin Park <paakjong@voyager.fm>
2026-03-24 00:20:58 +0900

    refactor(macos): EntryArrangementsAction의 delegate 계층을 중첩 enum으로 정리
    EntryArrangementsAction에서 기존 최상위 delegate enum을 내부 Delegate로 이관해 액션 네임스페이스를 정돈하고, 라우팅 의도 표현을 단일 구조로 통일한다
    
    requestApply와 applied 이벤트를 동일 의미를 유지한 채 구조적으로 재배치해 가독성과 TCA 소유권 해석을 개선한다
    
    최상위 delegate 선언 제거로 EntryArrangementsAction의 타입 계층을 단일 형태로 정리해 후속 리팩터링 비용을 낮춘다

--- apps/macos/Voyager/Voyager/04_Features/EntryArrangements/Model/EntryArrangementsAction.swift
+++ apps/macos/Voyager/Voyager/04_Features/EntryArrangements/Model/EntryArrangementsAction.swift
@@ -9,13 +9,13 @@ enum EntryArrangementsAction: CasePathable, Equatable, Sendable {
     case setGroupKey(GroupKey)
     case toggleCollapsedGroup(String)
     case reapply
-
-    case delegate(EntryArrangementsDelegate)
     case apply(items: [EntryModel], isCollectionMode: Bool)
-}
 
-@CasePathable
-enum EntryArrangementsDelegate: CasePathable, Equatable, Sendable {
-    case requestApply
-    case applied(sortedItems: [EntryModel], isCollectionMode: Bool)
+    case delegate(Delegate)
+
+    @CasePathable
+    enum Delegate: CasePathable, Equatable, Sendable {
+        case requestApply
+        case applied(sortedItems: [EntryModel], isCollectionMode: Bool)
+    }
 }
