b70fa1f06b6f2811b5510296a821dd4254ffba03
OHWONSIK <wonsik@voyager.fm>
2026-03-25 16:06:02 +0900

    refactor(macos): recent/tag 검색용 XPC 계약 모델을 분리 - recent/tag 전용 request/response payload를 shared contract에 추가 - 엔트리 메타데이터 전달용 payload를 경계 모델로 정리 - 기존 filter/query 계약을 유지한 채 recent/tag seam을 확장

--- apps/macos/Voyager/Shared/Models/FilterSearchXPCProtocol.swift
+++ apps/macos/Voyager/Shared/Models/FilterSearchXPCProtocol.swift
@@ -4,6 +4,8 @@ import Foundation
 protocol FilterSearchXPCServiceProtocol {
     nonisolated func applyFilters(_ requestData: Data, withReply reply: @escaping (Data?, NSError?) -> Void)
     nonisolated func querySearch(_ requestData: Data, withReply reply: @escaping (Data?, NSError?) -> Void)
+    nonisolated func recentSearch(_ requestData: Data, withReply reply: @escaping (Data?, NSError?) -> Void)
+    nonisolated func tagSearch(_ requestData: Data, withReply reply: @escaping (Data?, NSError?) -> Void)
 }
 
 enum FilterSearchXPCServiceConstants {
