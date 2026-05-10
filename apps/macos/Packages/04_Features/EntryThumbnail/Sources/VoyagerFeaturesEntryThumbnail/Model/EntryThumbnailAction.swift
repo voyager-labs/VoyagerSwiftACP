3ca577248fd613691b80af5b58a715a10f3e78a0
Jongmin Park <paakjong@voyager.fm>
2026-05-10 16:52:01 +0900

    refactor(features): move entry thumbnail into package

--- apps/macos/Packages/04_Features/EntryThumbnail/Sources/VoyagerFeaturesEntryThumbnail/Model/EntryThumbnailAction.swift
+++ apps/macos/Packages/04_Features/EntryThumbnail/Sources/VoyagerFeaturesEntryThumbnail/Model/EntryThumbnailAction.swift
@@ -2,7 +2,7 @@ import ComposableArchitecture
 import Foundation
 
 @CasePathable
-enum EntryThumbnailAction: CasePathable, Sendable {
+public enum EntryThumbnailAction: CasePathable, Sendable {
     case requestThumbnails(paths: [String])
     case thumbnailsReady(paths: [String])
     case thumbnailRequestFailed(paths: [String])
